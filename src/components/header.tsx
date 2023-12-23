9768485f4e03b385d59a92ac0c194a0555324aca
Brian Adhitya <brianadhitya@Brians-MacBook-Pro.local>
2023-12-24 07:36:21 +0800

    Minor changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,10 @@
 
 import Link from "next/link";
 import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header: React.FC = () => {
+  const { status } = useSession();
   return (
     <header>
       <nav className="bg-gray-200 border-gray-900 px-4 py-2">
@@ -18,12 +20,21 @@ const Header: React.FC = () => {
             >
               About
             </a>
-            <a
-              href={"/contact"}
-              className="text-gray-800 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-70 focus:outline-none dark:focus:ring-gray-800"
-            >
-              Contact
-            </a>
+            {status === "authenticated" ? (
+              <button
+                onClick={() => signOut()}
+                className="text-gray-800 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-70 focus:outline-none dark:focus:ring-gray-800"
+              >
+                Sign out
+              </button>
+            ) : (
+              <button
+                onClick={() => signIn("google")}
+                className="text-gray-800 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-70 focus:outline-none dark:focus:ring-gray-800"
+              >
+                Sign in
+              </button>
+            )}
           </div>
         </div>
       </nav>
